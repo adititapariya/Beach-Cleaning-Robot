78ed15ea9ab209eb5685795a7d4c90417716633d
Harshil Mistry <109202024+HarshilMistry23@users.noreply.github.com>
2024-07-21 20:34:42 +0530

    Add files via upload

---
+++ Waste Segregation & Circuit testing/Capacitive Sensors/capiproxsensor.ino
@@ -0,0 +1,62 @@
+ // Capacitive Proximity Sensor LJC18A3-B-Z/AX Test for Different Materials
+//Black - data, blue - ground, Red - VCC
+
+const int sensorPin = A0; // Connect the sensor output to Arduino digital pin 2
+int sensorThreshold = 0; // Variable to store the sensor threshold for different materials
+int sensorValue = 0;     // Variable to store the sensor reading
+int PWM = 8;
+int DIR = 10;
+
+void calibrateSensor();
+void setup() {
+  pinMode(8,OUTPUT);
+  pinMode(10,OUTPUT);
+  Serial.begin(9600);    // Initialize serial communication at 9600 baud
+  calibrateSensor();     // Call the calibration function during setup
+}
+
+void loop() {
+  // Read the sensor value
+  sensorValue = analogRead(sensorPin);
+//  Serial.print("Value = ");
+//  Serial.println(sensorValue);
+   digitalWrite(DIR,HIGH);
+   analogWrite(PWM,80);
+  // Compare the sensor value with the calibrated threshold
+  if (sensorValue == LOW) {
+    Serial.println("No object detected.");
+  } 
+  else if(sensorValue == 1022 || sensorValue == 1023) {
+    Serial.println("Other Material");
+  }
+  else {
+    Serial.print("Plastic Detected : ");
+    Serial.println(sensorValue);
+  }
+  delay(500); // You can adjust the delay to change the sensor reading rate
+
+}
+
+
+void calibrateSensor() {
+  Serial.println("Calibrating the sensor. Please remove all objects from the sensor area.");
+  delay(5000); // Allow time to clear the sensor area for calibration
+  
+  int sum = 0;
+  const int numReadings = 50; // Number of readings to average for calibration
+  
+  for (int i = 0; i < numReadings; i++) {
+    sum += analogRead(sensorPin);
+    delay(50); // Add a small delay between readings
+  }
+  
+  int baseline = sum / numReadings;
+  
+  Serial.print("Calibration complete. Baseline value: ");
+  Serial.println(baseline);
+
+  
+  
+  sensorThreshold = baseline -30; // Adjust the threshold as needed based on the material being detected
+  // You may need to adjust the threshold value based on your specific setup and materials.
+}
